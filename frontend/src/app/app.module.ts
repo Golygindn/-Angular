3862ae555bf190b1ffcd30e17b8072d4f48b3d50
danilagolygin <t0j8kzqboh@gmail.com>
2019-10-31 17:51:00 +0300

    Сверстал главную страницу

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { VacancyComponent } from './components/vacancy/vacancy.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    VacancyComponent,
+    ContactsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
